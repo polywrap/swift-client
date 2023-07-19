f6ea8b390af71ea9caaf6168d3cac15125ef8c79
Cesar <cesarbrazon10@gmail.com>
2023-07-19 17:09:15 +0200

    chore: remove unneeded dependency

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/hirotakan/MessagePacker.git", from: "0.4.7"),
-        .package(url: "https://github.com/SwiftyLab/AsyncObjects.git", from: "2.1.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+        .package(url: "https://github.com/SwiftyLab/AsyncObjects.git", from: "2.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
