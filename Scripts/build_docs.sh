f361a5ce462fff7f99d38d688adddcb83f389507
Cesar <cesarbrazon10@gmail.com>
2023-07-19 15:20:58 +0200

    chore: another try of doc in gh pages

--- Scripts/build_docs.sh
+++ Scripts/build_docs.sh
@@ -1,23 +1,4 @@
 set -e # Helps to give error info
 
-cd ../
-
-# xcodebuild docbuild -scheme PolywrapClient -derivedDataPath ./DocsTemp -destination 'platform=macOS'
-
-# echo "Copying DocC archives to doc_archives..."
-
-# mkdir -p doc_archives
-
-# cp -R `find DocsTemp -type d -name "*.doccarchive"` doc_archives
-
-# mkdir -p docs
-
-# for ARCHIVE in doc_archives/*.doccarchive; do
-#     cmd() {
-#         echo "$ARCHIVE" | awk -F'.' '{print $1}' | awk -F'/' '{print tolower($2)}'
-#     }
-#     ARCHIVE_NAME="$(cmd)"
-#     echo "Processing Archive: $ARCHIVE"
-#     echo "Archive name: $ARCHIVE_NAME"
-# done
-$(xcrun --find docc) process-archive transform-for-static-hosting doc_archives/PolywrapClient.doccarchive --hosting-base-path polywrapclient --output-path docs/polywrapclient
+xcodebuild clean docbuild -scheme PolywrapClient -derivedDataPath ./BuiltDocs -destination 'platform=macOS'
+$(xcrun --find docc) process-archive transform-for-static-hosting ./BuiltDocs/Build/Products/Debug/PolywrapClient.doccarchive --hosting-base-path swift-client --output-path docs/
