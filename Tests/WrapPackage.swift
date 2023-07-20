dcc755b8a74927b0da88fc3e7d0d051cafc41994
Cesar <cesarbrazon10@gmail.com>
2023-07-20 23:19:32 +0200

    test: wrap package with module

--- Tests/WrapPackage.swift
+++ Tests/WrapPackage.swift
@@ -10,20 +10,27 @@ import Foundation
 import PolywrapClient
 
 class WrapPackageTests: XCTestCase {
-    let reader = ResourceReader(bundle: Bundle.module, path: "Cases/subinvoke/00-subinvoke/implementations/as")
-    func testWrapPackage() throws {
+    let reader = ResourceReader(bundle: Bundle.module, path: "Cases/no-args/implementations/rs")
+    func testWrapPackageWithReader() throws {
         let package = WasmPackage(reader: reader, module: nil)
         let wrapper = try package.createWrapper()
         XCTAssert(wrapper is WasmWrapper)  // Ensure the type is correct.
     }
-    
+
+    func testWrapPackageWithModule() throws {
+        let module = try reader.readFile(nil)
+        let package = WasmPackage(reader: nil, module: module)
+        let wrapper = try package.createWrapper()
+        XCTAssert(wrapper is WasmWrapper)  // Ensure the type is correct.
+    }
+
     func testGetModuleThrowsWhenReaderMissing() {
         let package = WasmPackage(reader: nil, module: nil)
         XCTAssertThrowsError(try package.getModule()) { error in
             XCTAssertEqual(error as? WasmPackageError, WasmPackageError.readerMissing)
         }
     }
-    
+
     func testGetModuleReturnsStoredModule() throws {
         let expectedModule = [UInt8](arrayLiteral: 1, 2, 3)
         let package = WasmPackage(reader: nil, module: expectedModule)
