f1bbdfdf8ebada0d9df0fc13e1751d2155bd55d1
Cesar <cesarbrazon10@gmail.com>
2023-08-03 12:27:26 +0200

    chore: fix msgpack tests

--- Tests/MsgpackTests.swift
+++ Tests/MsgpackTests.swift
@@ -13,30 +13,23 @@ struct CustomStruct: Encodable, Equatable, Decodable {
 class MsgpackTests: XCTestCase {
     func testEncodeMapWithStruct() {
         let customStruct: Map<String, CustomStruct> = Map(["one": CustomStruct(1)])
-        do {
-            let encoded = try encode(value: customStruct)
-            print(encoded)
-            let decoded: Map<String, CustomStruct> = try decode(value: encoded)
-            print(decoded)
-        } catch {
-            print(error)
+        
+        guard let encoded = try? encode(value: customStruct),
+              let decoded: Map<String, CustomStruct> = try? decode(value: encoded)
+        else {
+            XCTFail("Encoding failed")
+            return
         }
-//        guard let encoded = try? encode(value: customStruct),
-//              let decoded: Map<String, CustomStruct> = try? decode(value: encoded)
-//        else {
-//            XCTFail("Encoding failed")
-//            return
-//        }
-//        XCTAssertEqual(decoded, customStruct)
+        XCTAssertEqual(decoded, customStruct)
     }
 
     func testEncodeDecodeMap() {
-        var customMap: [String: String] = [:]
-        customMap["firstKey"] = "firstValue"
-        customMap["secondKey"] = "secondValue"
+        var customMap: Map<String, String> = Map([:])
+        customMap.dictionary["firstKey"] = "firstValue"
+        customMap.dictionary["secondKey"] = "secondValue"
 
         guard let encoded = try? encode(value: customMap),
-              let decoded: [String: String] = try? decode(value: encoded)
+              let decoded: Map<String, String> = try? decode(value: encoded)
         else {
             XCTFail("Encoding or decoding failed")
             return
@@ -46,12 +39,12 @@ class MsgpackTests: XCTestCase {
     }
 
     func testEncodeDecodeNestedMap() {
-        var customMap: [String: [String: String]] = [:]
-        customMap["firstKey"] = ["one": "1"]
-        customMap["secondKey"] = ["second": "2"]
+        var customMap: Map<String, Map<String, String>> = Map([:])
+        customMap.dictionary["firstKey"] = Map(["one": "1"])
+        customMap.dictionary["secondKey"] = Map(["second": "2"])
 
         guard let encoded = try? encode(value: customMap),
-              let decoded: [String: [String: String]] = try? decode(value: encoded)
+              let decoded: Map<String, Map<String, String>> = try? decode(value: encoded)
         else {
             XCTFail("Encoding or decoding failed")
             return
@@ -62,25 +55,31 @@ class MsgpackTests: XCTestCase {
 
     func testEncodeDecodeComplexStructWithNestedMap() throws {
         struct ComplexStruct: Codable, Equatable {
-            let map: [String: Int]
-            let nestedMap: [String: [String: Int]]
+            let map: Map<String, Int>
+            let nestedMap: Map<String, Map<String, Int>>
         }
         
-        let map = ["Hello": 1, "Heyo": 50]
-        let nestedMap = ["nested": map]
+        let map = Map(["Hello": 1, "Heyo": 50])
+        let nestedMap = Map(["nested": map])
         
         let complexStruct = ComplexStruct(map: map, nestedMap: nestedMap)
-        let encoded = try encode(value: complexStruct)
-        XCTAssertEqual(encoded, [130, 163, 109, 97, 112, 199, 14, 1, 130, 165, 72, 101, 108, 108, 111, 1, 164, 72, 101, 121, 111, 50, 169, 110, 101, 115, 116, 101, 100, 77, 97, 112, 199, 25, 1, 129, 166, 78, 101, 115, 116, 101, 100, 199, 14, 1, 130, 165, 72, 101, 108, 108, 111, 1, 164, 72, 101, 121, 111, 50])
+        guard let encoded = try? encode(value: complexStruct),
+              let decoded: ComplexStruct = try? decode(value: encoded)
+        else {
+            XCTFail("Encoding or decoding failed")
+            return
+        }
+        XCTAssertEqual(decoded, complexStruct)
+
     }
 
     func testEncodeDecodeMapOfBytes() {
-        var customMap: [String: [UInt8]] = [:]
-        customMap["firstKey"] = [1, 2, 3]
-        customMap["secondKey"] = [3, 2, 1]
+        var customMap: Map<String, [UInt8]> = Map([:])
+        customMap.dictionary["firstKey"] = [1, 2, 3]
+        customMap.dictionary["secondKey"] = [3, 2, 1]
 
         guard let encoded = try? encode(value: customMap),
-              let decoded: [String: [UInt8]] = try? decode(value: encoded)
+              let decoded: Map<String, [UInt8]> = try? decode(value: encoded)
         else {
             XCTFail("Encoding or decoding failed")
             return
