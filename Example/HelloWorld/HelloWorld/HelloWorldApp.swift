d8c47ec41cadc3fde4fae60dc90eb4f55e5f0749
Cesar <cesarbrazon10@gmail.com>
2023-08-17 02:33:30 +0200

    chore(examples): hello world example added

---
+++ Example/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Cesar Brazon on 16.08.2023..
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
